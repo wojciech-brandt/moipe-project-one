d700da02674cce7df0a7c21b37c5186aea18230b
wojciech-brandt <wojciech.brandt.99@gmail.com>
2021-11-11 17:58:16 +0100

    mostly new functions in statistics.py

--- statistics.py
+++ statistics.py
@@ -3,3 +3,27 @@ import numpy as np
 def border_values(arr: np.ndarray) -> tuple:
 	return (arr[0], arr[-1])
 
+def max_value(arr: np.ndarray):
+	max = 0
+	for i in arr:
+		if i > max: max = i
+	
+	return max
+
+def min_value(arr: np.ndarray):
+	min = 0
+	for i in arr:
+		if i < min: min = i
+
+	return min
+
+def arr_sum(arr: np.ndarray):
+	sum = 0
+	for i in arr:
+		sum = sum + i
+	
+	return sum
+
+def arr_average(arr: np.ndarray): return arr_sum(arr)/arr.size
+
+
