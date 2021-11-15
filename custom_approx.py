8c98eb6ce8e04046e6f90c0e5b3aef382e36b177
wojciech-brandt <wojciech.brandt.99@gmail.com>
2021-11-15 02:39:51 +0100

    final commit before first check-up

--- custom_approx.py
+++ custom_approx.py
@@ -25,3 +25,18 @@ def natlog_approximation(
 
 	A_vec = np.linalg.inv( np.transpose(X_vec) @ X_vec ) @ np.transpose(X_vec) @ Yarr
 	return (X_vec @ A_vec)
+
+def hiperbolic_approximation(
+	Xarr: np.ndarray,
+	Yarr: np.ndarray,
+	deg: np.int8
+) -> np.ndarray:
+
+	X_vec = np.zeros([Yarr.size, deg])
+
+	for i in np.arange(deg):
+		X_vec[:, i] = np.reciprocal(Xarr)**i
+
+	A_vec = np.linalg.inv( np.transpose(X_vec) @ X_vec ) @ np.transpose(X_vec) @ Yarr
+	return (X_vec @ A_vec)
+
